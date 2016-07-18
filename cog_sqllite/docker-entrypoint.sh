033a0ce5d9c8d1ab71ab6ec0d25559e89bd4a99b
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-07-18 14:41:52 -0600

    Using docker-entrypoint for CoG qith sqllite backend.

---
+++ cog_sqllite/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# use virtualenv
+source $COG_DIR/venv/bin/activate
+
+# upgrade CoG
+cd $COG_INSTALL_DIR
+python setup.py -q setup_cog --esgf=false
+
+# customize CoG settings
+export DOCKER_IP=$@
+echo "Using DOCKER_IP=$DOCKER_IP"
+sed -i 's/ALLOWED_HOSTS = .*/ALLOWED_HOSTS = '"${DOCKER_IP}"'/g' $COG_CONFIG_DIR/cog_settings.cfg
+
+# PRODUCTION_SERVER = True would require use of SSL to transmit any cookie
+sed -i 's/PRODUCTION_SERVER = True/PRODUCTION_SERVER = False/g' $COG_CONFIG_DIR/cog_settings.cfg
+
+# Start CoG in virtual environment
+python ./manage.py runserver 0.0.0.0:8000
