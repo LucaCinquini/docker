6e40c39cd14a95af8e519bdeb6276b8e0d12a124
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-07-18 14:53:50 -0600

    Adding entrypoint to CoG for ESGF.

--- cog_esgf/docker-entrypoint.sh
+++ cog_esgf/docker-entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# use virtualenv
+source $COG_DIR/venv/bin/activate
+
+# start postgres
+su -c 'pg_ctl start -D /var/lib/pgsql/data' postgres
+
+# upgrade CoG
+cd $COG_INSTALL_DIR
+python setup.py -q setup_cog --esgf=true
+
 # customize CoG settings
 export DOCKER_IP=$@
-sed -i 's/ALLOWED_HOSTS = localhost/ALLOWED_HOSTS = '"${DOCKER_IP}"', localhost/g' $COG_CONFIG_DIR/cog_settings.cfg
+echo "Using DOCKER_IP=$DOCKER_IP"
+sed -i 's/ALLOWED_HOSTS = .*/ALLOWED_HOSTS = '"${DOCKER_IP}"'/g' $COG_CONFIG_DIR/cog_settings.cfg
+
+# PRODUCTION_SERVER = True would require use of SSL to transmit any cookie
+sed -i 's/PRODUCTION_SERVER = True/PRODUCTION_SERVER = False/g' $COG_CONFIG_DIR/cog_settings.cfg
 
 # Start CoG in virtual environment
-source $COG_DIR/venv/bin/activate
 cd $COG_INSTALL_DIR
 python ./manage.py runserver 0.0.0.0:8000
