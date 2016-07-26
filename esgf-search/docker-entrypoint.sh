a1632906df2adaa3c7996c0040b118fec9bdfb66
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-07-26 13:38:43 -0600

    Inserting first version of ESGF search application.

---
+++ esgf-search/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# use Docker Host IP address
+export DOCKER_IP=$@
+echo "Using DOCKER_IP=$DOCKER_IP"
+sed -i 's/localhost/'"${DOCKER_IP}"'/g' /esg/config/esgf.properties
+
+cd $CATALINA_HOME/bin
+./catalina.sh start
+
+cd $SOLR_INSTALL_DIR/bin
+export SOLR_INCLUDE=${SOLR_HOME}/solr.in.sh
+./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/master-8984 -p 8984 -a '-Denable.master=true'
+./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/slave-8983 -p 8983 -a '-Denable.slave=true'
+
+# keep container running
+tail -f $CATALINA_HOME/logs/catalina.out
