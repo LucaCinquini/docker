6a9b62dbd9b77f295aba4e42afe241aec7bfb471
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-07-26 15:28:44 -0600

    Adding start.sh

--- esgf-search/docker-entrypoint.sh
+++ esgf-search/docker-entrypoint.sh
@@ -5,18 +5,8 @@ export DOCKER_IP=$@
 echo "Using DOCKER_IP=$DOCKER_IP"
 sed -i 's/localhost/'"${DOCKER_IP}"'/g' /esg/config/esgf.properties
 
-# start Tomcat
-cd $CATALINA_HOME/bin
-./catalina.sh start
-
-# start Solrs
-cd $SOLR_INSTALL_DIR/bin
-export SOLR_INCLUDE=${SOLR_HOME}/solr.in.sh
-./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/master-8984 -p 8984 -a '-Denable.master=true'
-./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/slave-8983 -p 8983 -a '-Denable.slave=true'
-
-# start Apache httpd
-service httpd restart
+# start services
+/usr/local/bin/start.sh
 
 # keep container running
 tail -f $CATALINA_HOME/logs/catalina.out
---
+++ esgf-search/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# start Tomcat
+cd $CATALINA_HOME/bin
+./catalina.sh start
+
+# start Solrs
+cd $SOLR_INSTALL_DIR/bin
+export SOLR_INCLUDE=${SOLR_HOME}/solr.in.sh
+./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/master-8984 -p 8984 -a '-Denable.master=true'
+./solr start -d $SOLR_INSTALL_DIR/server -s $SOLR_HOME/slave-8983 -p 8983 -a '-Denable.slave=true'
+
+# start Apache httpd
+service httpd start
