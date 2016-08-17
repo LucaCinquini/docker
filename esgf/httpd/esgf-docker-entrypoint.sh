5e2ca69759d2a990fdb86dc21962f00ef7c2a709
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-08-17 10:37:59 -0600

    Updating the httpd configuration for ESGF

--- esgf/httpd/esgf-docker-entrypoint.sh
+++ esgf/httpd/esgf-docker-entrypoint.sh
@@ -4,6 +4,10 @@
 # change CoG directory permission
 chown -R apache:apache /usr/local/cog
 
+# remove conflicting configuration
+rm -f /etc/httpd/conf.d/ssl.conf
+rm -f /etc/httpd/conf.d/welcome.conf
+
 # start httpd service
 service httpd restart
 
