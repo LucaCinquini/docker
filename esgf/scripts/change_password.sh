c379c5b6bd82451adc3c2aef3cc274d819ac54de
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-09-13 08:42:10 -0600

    Working on Tomcat TDS authentication.

--- esgf/scripts/change_password.sh
+++ esgf/scripts/change_password.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # script to change the root ESGF password
 # the new password is obtained from the env variable ESGF_PASSWORD
-# files to be changed are located under $ESGF_HOME/config
+# files to be changed are located under $ESGF_HOME/config, $COG_CONFIG_DIR
 
 # check for required environment
+# FIXME: consolidate
 if [ $ESGF_PASSWORD == "" ];
 then
    echo "Env variable ESGF_PASSWORD not set, exiting"
@@ -14,10 +15,21 @@ then
    echo "Env variable ESGF_HOME not set, exiting"
    exit -1
 fi
+if [ $COG_CONFIG_DIR == "" ];
+then
+   echo "Env variable COG_CONFIG_DIR not set, exiting"
+   exit -1
+fi
 
 # change password inside esgf_postgres container
 docker exec -it esgf_postgres /bin/bash -c "export ESGF_PASSWORD=${ESGF_PASSWORD} && /usr/share/bin/change_password.sh"
 
 # change password in common ESGF configuration files under $ESGF_HOME
+# FIXME: run inside container
 echo ${ESGF_PASSWORD} > ${ESGF_HOME}/config/.esg_pg_pass
 echo ${ESGF_PASSWORD} > ${ESGF_HOME}/config/.esgf_pass
+
+# change password in CoG settings file
+# FIXME: run inside httpd container
+sed -i -- 's/DATABASE_PASSWORD = .*/DATABASE_PASSWORD = '"${ESGF_PASSWORD}"'/g' $COG_CONFIG_DIR/cog_settings.cfg
+sed -i -- 's/dbsuper:.*@esgf_postgres_app/dbsuper:'"${ESGF_PASSWORD}"'@esgf_postgres_app/g' $COG_CONFIG_DIR/cog_settings.cfg
