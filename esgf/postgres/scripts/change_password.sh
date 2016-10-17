2c5fd055448c3739a09985ae44f19ac7d2a40144
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2016-10-17 14:48:36 -0600

    Improving on changing the password.

--- esgf/postgres/scripts/change_password.sh
+++ esgf/postgres/scripts/change_password.sh
@@ -12,10 +12,15 @@ fi
 echo "ALTER USER dbsuper WITH ENCRYPTED PASSWORD '${ESGF_PASSWORD}';" > ./change_password.sql
 echo "ALTER USER esgcet WITH ENCRYPTED PASSWORD '${ESGF_PASSWORD}';" >> ./change_password.sql
 psql -U dbsuper esgcet < ./change_password.sql
-# clean up
-rm ./change_password.sql
 
 # set new password in /root/.pgpass
-echo "localhost:5432:cogdb:dbsuper:${ESGF_PASSWORD}" > /root/.pgpass
-echo "localhost:5432:esgcet:dbsuper:${ESGF_PASSWORD}" >> /root/.pgpass
-chmod 0600 /root/.pgpass
+# but only if previous command succeded!
+if [ $? == 0 ]
+then
+   echo "localhost:5432:cogdb:dbsuper:${ESGF_PASSWORD}" > /root/.pgpass
+   echo "localhost:5432:esgcet:dbsuper:${ESGF_PASSWORD}" >> /root/.pgpass
+   chmod 0600 /root/.pgpass
+fi
+
+# clean up
+rm ./change_password.sql
--- esgf/scripts/change_password.sh
+++ esgf/scripts/change_password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to change the root ESGF password
 # the new password is obtained from the env variable ESGF_PASSWORD
-# most password changes are executed directly inside each container
+# some password changes are executed directly inside each container
 
 if [ "${ESGF_PASSWORD}" = "" ] || [ "${ESGF_CONFIG}" = "" ];
 then
@@ -9,17 +9,23 @@ then
    exit -1
 fi
 
+# change password inside (running) postgres container
+docker start postgres
+# give postgres time to start
+sleep 3
+docker exec -it postgres /bin/bash -c "export ESGF_PASSWORD=${ESGF_PASSWORD} && /usr/local/bin/change_password.sh"
+docker stop postgres
+
 # change password in common ESGF configuration files under $ESGF_CONFIG/esg/config
 echo ${ESGF_PASSWORD} > ${ESGF_CONFIG}/esg/config/.esg_pg_pass
 echo ${ESGF_PASSWORD} > ${ESGF_CONFIG}/esg/config/.esgf_pass
 sed -i -- 's/db.password=.*/db.password='"${ESGF_PASSWORD}"'/g' ${ESGF_CONFIG}/esg/config/esgf.properties
 
-# must first change cog password inside esgf_httpd container
-# before the postgres password changes
-docker exec -it esgf_httpd /bin/bash -c "export ESGF_PASSWORD=${ESGF_PASSWORD} && /usr/local/bin/change_cog_password.sh"
+# change password to access the postgres databases in CoG settings file
+sed -i -- 's/DATABASE_PASSWORD = .*/DATABASE_PASSWORD = '"${ESGF_PASSWORD}"'/g' $ESGF_CONFIG/cog/cog_config/cog_settings.cfg
+sed -i -- 's/dbsuper:.*@esgf-postgres/dbsuper:'"${ESGF_PASSWORD}"'@esgf-postgres/g' $ESGF_CONFIG/cog/cog_config/cog_settings.cfg
 
-# change password inside esgf_postgres container
-docker exec -it esgf_postgres /bin/bash -c "export ESGF_PASSWORD=${ESGF_PASSWORD} && /usr/local/bin/change_password.sh"
-
-# change password inside esgf-data-node container
-docker exec -it esgf-data-node /bin/bash -c "export ESGF_PASSWORD=${ESGF_PASSWORD} && /usr/local/bin/change_data_node_password.sh"
+# change password inside (running) data-node container
+docker start data-node
+docker exec -it data-node /bin/bash -c "export ESGF_PASSWORD=${ESGF_PASSWORD} && /usr/local/bin/change_data_node_password.sh"
+docker stop data-node
